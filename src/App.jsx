81dc90607a4d2fc897cc9f64548907333ebdefac
jiratchaya premsil <jiratchaya5923@gmail.com>
2026-01-05 13:53:59 +0700

    feature: add ProfileCard components and pass data via props

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import ProfileCard from "./components/ProfileCard"
 function App() {
   return (
-    <div>
+    <div style={{ display: 'flex' ,  flexDirection: 'column', alignItems:'center'}}>
       <h1>My first react app</h1>
+      <ProfileCard
+        name="John Doe"
+        role="Software Engineer"
+        bio="Passionate about creating efficient and scalable web applications."
+      />
+      <ProfileCard
+        name="Jane Smith"
+        role="bakery"
+        bio="i love my husbant's cake"
+      />
     </div>
   )
 }
