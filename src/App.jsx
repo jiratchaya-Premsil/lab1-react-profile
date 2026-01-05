2dd961576e98608ada802cf6c7fbb82d34660546
jiratchaya premsil <jiratchaya5923@gmail.com>
2026-01-05 14:13:01 +0700

    feat: implement conter state and github api fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
 import ProfileCard from "./components/ProfileCard"
+import {useState, useEffect} from 'react';
 function App() {
+  const [githubData, setGithubData] = useState(null);
+  const usernmae = "jiratchaya-Premsil";
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${usernmae}`)
+      .then(response => response.json())
+      .then(data => {
+        setGithubData(data);
+        console.log(data);
+      })
+      .catch(error => console.error(error))
+  },[]);
   return (
     <div style={{ display: 'flex' ,  flexDirection: 'column', alignItems:'center'}}>
       <h1>My first react app</h1>
+
       <ProfileCard
-        name="John Doe"
-        role="Software Engineer"
-        bio="Passionate about creating efficient and scalable web applications."
+        name={githubData.login}
+        role={githubData.type}
+        bio={githubData.id}
       />
-      <ProfileCard
-        name="Jane Smith"
-        role="bakery"
-        bio="i love my husbant's cake"
-      />
+
     </div>
   )
 }
